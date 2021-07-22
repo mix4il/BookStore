62df891b1eda88a8e74828bf5aaaa47ede8561e4
Misha <basckackov.mixail2015@yandex.ru>
2021-07-22 07:09:43 +0300

    Добавил тесты

---
+++ NUnitTestProductService/UnitTest1.cs
@@ -0,0 +1,26 @@
+using BookStore.Services;
+using NUnit.Framework;
+
+namespace NUnitTestProductService
+{
+    public class ProductServiceTests
+    {
+
+
+        private IProductService _productService;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
